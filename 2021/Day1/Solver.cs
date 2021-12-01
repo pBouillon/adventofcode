bb5f4302b35742263b34e40fbfc9cef05a73949b
pBouillon <pro.pierre.bouillon@protonmail.com>
2021-12-01 16:53:32 +0100

    Add the first day of 2021

---
+++ 2021/Day1/Solver.cs
@@ -0,0 +1,41 @@
+﻿using Commons;
+
+using System;
+using System.IO;
+using System.Linq;
+
+namespace _2021.Day5;
+
+public class Solver : ISolver<int[], int>
+{
+
+    public string InputPath
+        => Path.Join(AppDomain.CurrentDomain.BaseDirectory, "Day1/input.txt");
+
+    public int PartOne(int[] measures)
+    {
+        var count = 0;
+        
+        for (var i = 1; i < measures.Length; ++i)
+        {
+            if (measures[i - 1] < measures[i]) ++count;
+        }
+
+        return count;
+    }
+
+    public int PartTwo(int[] measures)
+    {
+        var windowSums = Enumerable.Range(0, measures.Length - 2)
+            .Select(offset => measures[offset..(offset + 3)].Sum())
+            .ToArray();
+
+        return PartOne(windowSums);
+    }
+
+    public int[] ReadInput(string inputPath)
+        => File
+            .ReadLines(inputPath)
+            .Select(measure => int.Parse(measure))
+            .ToArray();
+}
---
+++ 2021/Day1/SolverTests.cs
@@ -0,0 +1,40 @@
+﻿using _2021.Day5;
+
+using Xunit;
+
+namespace _2021.Day1;
+
+public class SolverTests
+{
+    [Fact]
+    public void PartOneExamples()
+    {
+        var solver = new Solver();
+        var measures = new int[] { 199, 200, 208, 210, 200, 207, 240, 269, 260,263 };
+        Assert.Equal(7, solver.PartOne(measures));
+    }
+
+    [Fact]
+    private void PartOneSolution()
+    {
+        var solver = new Solver();
+        var measures = solver.ReadInput(solver.InputPath);
+        Assert.Equal(1696, solver.PartOne(measures));
+    }
+
+    [Fact]
+    public void PartTwoExamples()
+    {
+        var solver = new Solver();
+        var measures = new int[] { 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 };
+        Assert.Equal(5, solver.PartTwo(measures));
+    }
+
+    [Fact]
+    private void PartTwoSolution()
+    {
+        var solver = new Solver();
+        var measures = solver.ReadInput(solver.InputPath);
+        Assert.Equal(1737, solver.PartTwo(measures));
+    }
+}
