bba116ff83f765ea51748585b88e844b8b9ae2c9
pBouillon <pro.pierre.bouillon@protonmail.com>
2021-12-02 07:40:55 +0100

    Add the second day of 2021

---
+++ 2021/Day2/Solver.cs
@@ -0,0 +1,70 @@
+﻿using Commons;
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _2021.Day2;
+
+public record Command(string Name, int Magnitude);
+
+public record Coordinate(int X, int Y);
+
+public record AimCoordinate(int X, int Y, int Aim);
+
+public class Solver : ISolver<IEnumerable<Command>, int>
+{
+    public string InputPath
+        => "Day2/input.txt";
+
+    public int PartOne(IEnumerable<Command> input)
+    {
+        var position = new Coordinate(0, 0);
+
+        foreach (var command in input)
+        {
+            position = command switch
+            {
+                { Name: "forward" } => position with { X = position.X + command.Magnitude },
+                { Name: "down" } => position with { Y = position.Y - command.Magnitude },
+                { Name: "up" } => position with { Y = position.Y + command.Magnitude },
+                _ => throw new System.Exception("Unknown command"),
+            };
+        }
+
+        var depth = position.Y * -1;
+        return position.X * depth;
+    }
+
+    public int PartTwo(IEnumerable<Command> input)
+    {
+        var position = new AimCoordinate(0, 0, 0);
+
+        foreach (var command in input)
+        {
+            position = command switch
+            {
+                { Name: "forward" } => position with
+                {
+                    X = position.X + command.Magnitude,
+                    Y = position.Y + position.Aim * command.Magnitude,
+                },
+                { Name: "down" } => position with { Aim = position.Aim - command.Magnitude },
+                { Name: "up" } => position with { Aim = position.Aim + command.Magnitude },
+                _ => throw new System.Exception("Unknown command"),
+            };
+        }
+
+        var depth = position.Y * -1;
+        return position.X * depth;
+    }
+
+    public IEnumerable<Command> ReadInput(string inputPath)
+        => File
+            .ReadLines(inputPath)
+            .Select(input =>
+            {
+                var splitted = input.Split(' ');
+                return new Command(splitted[0], int.Parse(splitted[1]));
+            });
+}
---
+++ 2021/Day2/SolverTests.cs
@@ -0,0 +1,54 @@
+﻿using Xunit;
+
+namespace _2021.Day2;
+
+public class SolverTests
+{
+    [Fact]
+    public void PartOneExamples()
+    {
+        var solver = new Solver();
+        var commands = new Command[] 
+        {
+            new Command("forward", 5),
+            new Command("down", 5),
+            new Command("forward", 8),
+            new Command("up", 3),
+            new Command("down", 8),
+            new Command("forward", 2),
+        };
+        Assert.Equal(150, solver.PartOne(commands));
+    }
+
+    [Fact]
+    private void PartOneSolution()
+    {
+        var solver = new Solver();
+        var commands = solver.ReadInput(solver.InputPath);
+        Assert.Equal(1728414, solver.PartOne(commands));
+    }
+
+    [Fact]
+    public void PartTwoExamples()
+    {
+        var solver = new Solver();
+        var commands = new Command[]
+        {
+            new Command("forward", 5),
+            new Command("down", 5),
+            new Command("forward", 8),
+            new Command("up", 3),
+            new Command("down", 8),
+            new Command("forward", 2),
+        };
+        Assert.Equal(900, solver.PartTwo(commands));
+    }
+
+    [Fact]
+    private void PartTwoSolution()
+    {
+        var solver = new Solver();
+        var commands = solver.ReadInput(solver.InputPath);
+        Assert.Equal(1765720035, solver.PartTwo(commands));
+    }
+}
