46827a1a8dc0c27b409e957140cae12c58b9f5f8
pBouillon <pro.pierre.bouillon@protonmail.com>
2021-12-06 06:50:48 +0100

    WIP 6th day of december

---
+++ 2021/Day6/Solver.cs
@@ -0,0 +1,71 @@
+﻿using Commons;
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _2021.Day6;
+
+public class LanternFish
+{
+    public int InternalTimer { get; private set; }
+
+    public LanternFish(int internalTimer)
+        => InternalTimer = internalTimer;
+
+    public void PassDay()
+        => InternalTimer = InternalTimer == 0
+            ? InternalTimer = 6
+            : --InternalTimer;
+}
+
+public class LanternFishPopulation
+{
+    public List<LanternFish> Population { get; private set; } = new List<LanternFish>();
+
+    public LanternFishPopulation(List<LanternFish> initialPopulation)
+        => Population = initialPopulation;
+
+    public void PassDay()
+    {
+        for (var i = 0; i < Population.Count(fish => fish.InternalTimer == 0); ++i)
+        {
+            Population.Add(new LanternFish(8));
+        }
+
+        Population.ForEach(fish => fish.PassDay());
+    }
+}
+
+public class Solver : ISolver<LanternFishPopulation, int>
+{
+    public string InputPath
+        => "Day6/input.txt";
+
+    public int PartOne(LanternFishPopulation lanternFishesPopulation)
+    {
+        for (int i = 0; i < 80; ++i)
+        {
+            lanternFishesPopulation.PassDay();
+        }
+
+        return lanternFishesPopulation.Population.Count;
+    }
+
+    public int PartTwo(LanternFishPopulation input)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public LanternFishPopulation ReadInput(string inputPath)
+    {
+        var lanternFishes = File
+            .ReadLines(inputPath)
+            .First()
+            .Split(",")
+            .Select(internalTimer => new LanternFish(int.Parse(internalTimer)))
+            .ToList();
+
+        return new LanternFishPopulation(lanternFishes);
+    }
+}
---
+++ 2021/Day6/SolverTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+using Xunit;
+
+namespace _2021.Day6;
+
+public class SolverTests
+{
+    [Fact]
+    public void PartOneExample()
+    {
+        var solver = new Solver();
+
+        var initialPopulation = new LanternFishPopulation(new List<LanternFish>
+        {
+            new LanternFish(3),
+            new LanternFish(4),
+            new LanternFish(3),
+            new LanternFish(1),
+            new LanternFish(2),
+        });
+
+        Assert.Equal(5934, solver.PartOne(initialPopulation));
+    }
+
+    [Fact]
+    public void PartOne()
+    {
+        var solver = new Solver();
+        var initialPopulation = solver.ReadInput(solver.InputPath);
+        Assert.Equal(6311, solver.PartOne(initialPopulation));
+    }
+}
+
