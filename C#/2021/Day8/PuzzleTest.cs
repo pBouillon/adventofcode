d040bd592f7f342f5e613d40d6d51af0f076d8c4
pBouillon <pro.pierre.bouillon@protonmail.com>
2021-12-08 06:44:19 +0100

    Add the eigthth day of december - part one

---
+++ C#/2021/Day8/PuzzleTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+using TestCommons;
+
+namespace _2021.Day8;
+
+public class PuzzleTest : SolverTest<Solver, IEnumerable<Entry>, int>
+{
+    protected override Example PartOne => new()
+    {
+        Input = new List<Entry>()
+        {
+            new Entry(new [] { "be", "cfbegad", "cbdgef", "fgaecd", "cgeb", "fdcge", "agebfd", "fecdb", "fabcd", "edb" }, new string[] { "fdgacbe", "cefdb", "cefbgd", "gcbe" }),
+            new Entry(new [] { "edbfga", "begcd", "cbg", "gc", "gcadebf", "fbgde", "acbgfd", "abcde", "gfcbed", "gfec" }, new string[] { "fcgedb", "cgb", "dgebacf", "gc" }),
+            new Entry(new [] { "fgaebd", "cg", "bdaec", "gdafb", "agbcfd", "gdcbef", "bgcad", "gfac", "gcb", "cdgabef" }, new string[] { "cg", "cg", "fdcagb", "cbg" }),
+            new Entry(new [] { "fbegcd", "cbd", "adcefb", "dageb", "afcb", "bc", "aefdc", "ecdab", "fgdeca", "fcdbega" }, new string[] { "efabcd", "cedba", "gadfec", "cb" }),
+            new Entry(new [] { "aecbfdg", "fbg", "gf", "bafeg", "dbefa", "fcge", "gcbea", "fcaegb", "dgceab", "fcbdga" }, new string[] { "gecf", "egdcabf", "bgf", "bfgea" }),
+            new Entry(new [] { "fgeab", "ca", "afcebg", "bdacfeg", "cfaedg", "gcfdb", "baec", "bfadeg", "bafgc", "acf" }, new string[] { "gebdcfa", "ecba", "ca", "fadegcb" }),
+            new Entry(new [] { "dbcfg", "fgd", "bdegcaf", "fgec", "aegbdf", "ecdfab", "fbedc", "dacgb", "gdcebf", "gf" }, new string[] { "cefg", "dcbef", "fcge", "gbcadfe" }),
+            new Entry(new [] { "bdfegc", "cbegaf", "gecbf", "dfcage", "bdacg", "ed", "bedf", "ced", "adcbefg", "gebcd" }, new string[] { "ed", "bcgafe", "cdgba", "cbgef" }),
+            new Entry(new [] { "egadfb", "cdbfeg", "cegd", "fecab", "cgb", "gbdefca", "cg", "fgcdab", "egfdb", "bfceg" }, new string[] { "gbdfcae", "bgc", "cg", "cgb" }),
+            new Entry(new [] { "gcafb", "gcf", "dcaebfg", "ecagb", "gf", "abcdeg", "gaef", "cafbge", "fdbac", "fegbdc" }, new string[] { "fgae", "cfgab", "fg", "bagce" })
+        },
+        Result = 26,
+    };
+
+    protected override int PartOneSolution => 0;
+
+    protected override Example PartTwo => throw new NotImplementedException();
+
+    protected override int PartTwoSolution => throw new NotImplementedException();
+}
---
+++ C#/2021/Day8/Solver.cs
@@ -0,0 +1,64 @@
+﻿using Commons;
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _2021.Day8;
+
+public record Digit(string Value, char[] Components);
+
+public record Entry(IEnumerable<string> SignalPatterns, IEnumerable<string> OutputValueDigits);
+
+public class Solver : ISolver<IEnumerable<Entry>, int>
+{
+    public string InputPath => "Day8/input.txt";
+
+    private readonly Dictionary<int, Digit> Digits = new Dictionary<int, Digit>
+    {
+        { 0, new Digit("0", new[] { 'a', 'b', 'c',      'e', 'f', 'g' })},  // 6 segments
+        { 1, new Digit("1", new[] {           'c',           'f'      })},  // 2 segments <- unique
+        { 2, new Digit("2", new[] { 'a',      'c', 'd', 'e',      'g' })},  // 5 segments
+        { 3, new Digit("3", new[] { 'a',      'c', 'd',      'f', 'g' })},  // 5 segments
+        { 4, new Digit("4", new[] {      'b', 'c', 'd',      'f'      })},  // 4 segments <- unique
+        { 5, new Digit("5", new[] { 'a', 'b',      'd',      'f', 'g' })},  // 5 segments
+        { 6, new Digit("6", new[] { 'a', 'b',      'd', 'e', 'f', 'g' })},  // 6 segments
+        { 7, new Digit("7", new[] { 'a',      'c',           'f'      })},  // 3 segments <- unique
+        { 8, new Digit("8", new[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g' })},  // 7 segments <- unique
+        { 9, new Digit("9", new[] { 'a', 'b', 'c', 'd',      'f', 'g' })},  // 6 segments
+    };
+
+    public int PartOne(IEnumerable<Entry> input)
+        => input
+            .Select(entry => entry.OutputValueDigits)
+            .Sum(digits => digits.Count(digit => new List<int>()
+                {
+                    Digits[1].Components.Length,
+                    Digits[4].Components.Length,
+                    Digits[7].Components.Length,
+                    Digits[8].Components.Length,
+                }
+                .Contains(digit.Distinct().Count())));
+    //.Count(digits => new List<int>()
+    //    {
+    //        Digits[1].Components.Length,
+    //        Digits[4].Components.Length,
+    //        Digits[7].Components.Length,
+    //        Digits[8].Components.Length,
+    //    }
+    //    .Contains(digits.Distinct().Count()));
+
+    public int PartTwo(IEnumerable<Entry> input)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public IEnumerable<Entry> ReadInput(string inputPath)
+        => File
+            .ReadLines(inputPath)
+            .Select(line =>
+            {
+                var parts = line.Split(" | ");
+                return new Entry(parts[0].Split(" "), parts[1].Split(" "));
+            });
+}
