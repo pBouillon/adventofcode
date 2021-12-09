582b038b56456431174394d8db070c440635dc1d
pBouillon <pro.pierre.bouillon@protonmail.com>
2021-12-09 06:43:14 +0100

    [skip ci] Add the ninth day of 2021 - part one

---
+++ C#/2021/Day9/PuzzleTest.cs
@@ -0,0 +1,36 @@
+﻿using TestCommons;
+
+namespace _2021.Day9;
+
+public class PuzzleTest : SolverTest<Solver, int[][], long>
+{
+    protected override Example PartOne => new()
+    {
+        Input = new[]
+        {
+            new[] { 2, 1, 9, 9, 9, 4, 3, 2, 1, 0 },
+            new[] { 3, 9, 8, 7, 8, 9, 4, 9, 2, 1 },
+            new[] { 9, 8, 5, 6, 7, 8, 9, 8, 9, 2 },
+            new[] { 8, 7, 6, 7, 8, 9, 6, 7, 8, 9 },
+            new[] { 9, 8, 9, 9, 9, 6, 5, 6, 7, 8 },
+        },
+        Result = 15,
+    };
+
+    protected override long PartOneSolution => 591;
+
+    protected override Example PartTwo => new()
+    {
+        Input = new[]
+        {
+            new[] { 2, 1, 9, 9, 9, 4, 3, 2, 1, 0 },
+            new[] { 9, 8, 5, 6, 7, 8, 9, 8, 9, 2 },
+            new[] { 8, 7, 6, 7, 8, 9, 6, 7, 8, 9 },
+            new[] { 9, 8, 9, 9, 9, 6, 5, 6, 7, 8 },
+        },
+        Result = 0,
+    };
+
+    protected override long PartTwoSolution => 0;
+}
+
---
+++ C#/2021/Day9/Solver.cs
@@ -0,0 +1,60 @@
+﻿using Commons;
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _2021.Day9;
+
+public class Solver : ISolver<int[][], long>
+{
+    public string InputPath => "Day9/input.txt";
+
+    private static bool IsLowPointCoordinate(int i, int j, int[][] grid)
+    {
+        var current = grid[i][j];
+        var neighbords = new Queue<int>();
+
+        // Top neighbor
+        if (i - 1 > -1) neighbords.Enqueue(grid[i - 1][j]);
+
+        // Down neighbor
+        if (i + 1 < grid.Length) neighbords.Enqueue(grid[i + 1][j]);
+
+        // Left neighbor
+        if (j - 1 > -1) neighbords.Enqueue(grid[i][j - 1]);
+
+        // Right neighbor
+        if (j + 1 < grid[i].Length) neighbords.Enqueue(grid[i][j + 1]);
+
+        return neighbords.All(neighbor => current < neighbor);
+    }
+
+    public long PartOne(int[][] input)
+    {
+        var lowestPoints = new Queue<int>();
+
+        for (var i = 0; i < input.Length; ++i)
+        {
+            for (var j = 0; j < input[i].Length; ++j)
+            {
+                if (IsLowPointCoordinate(i, j, input)) lowestPoints.Enqueue(input[i][j]);
+            }
+        }
+
+        return lowestPoints.Sum(value => value + 1);
+    }
+
+    public long PartTwo(int[][] input)
+    {
+        return 0;
+    }
+
+    public int[][] ReadInput(string inputPath)
+        => File
+            .ReadLines(inputPath)
+            .Select(line => line.Select(value => value.ToString())
+                .Select(int.Parse)
+                .ToArray())
+            .ToArray();
+}
