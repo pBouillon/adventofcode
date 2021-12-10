ece789ec2445bc24da3924eefa9208e0a9248ba8
pBouillon <pro.pierre.bouillon@protonmail.com>
2021-12-10 06:33:47 +0100

    [skip ci] - Add the thenth day of 2021 - part one

---
+++ C#/2021/Day10/PuzzleTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+using TestCommons;
+
+namespace _2021.Day10;
+
+public class PuzzleTest : SolverTest<Solver, IEnumerable<string>, int>
+{
+    private readonly IEnumerable<string> input = new List<string>
+    {
+        "[({(<(())[]>[[{[]{<()<>>",
+        "[(()[<>])]({[<{<<[]>>(",
+        "{([(<{}[<>[]}>{[]{[(<()>",
+        "(((({<>}<{<{<>}{[]{[]{}",
+        "[[<[([]))<([[{}[[()]]]",
+        "[{[{({}]{}}([{[{{{}}([]",
+        "{<[[]]>}<{[{[{[]{()[[[]",
+        "[<(<(<(<{}))><([]([]()",
+        "<{([([[(<>()){}]>(<<{{",
+        "<{([{{}}[<[[[<>{}]]]>[]]"
+    };
+
+    protected override Example PartOne => new()
+    {
+        Input = input,
+        Result = 26397,
+    };
+
+    protected override int PartOneSolution => 243939;
+
+    protected override Example PartTwo => new()
+    {
+        Input = input,
+        Result = 0,
+    };
+
+    protected override int PartTwoSolution => 0;
+}
---
+++ C#/2021/Day10/Solver.cs
@@ -0,0 +1,63 @@
+﻿using Commons;
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _2021.Day10;
+
+public class Solver : ISolver<IEnumerable<string>, int>
+{
+    public string InputPath => "Day10/input.txt";
+
+    private char? GetCorruptingChar(string line)
+    {
+        var closingSymbols = new Dictionary<char, char>()
+        {
+            ['('] = ')',
+            ['{'] = '}',
+            ['['] = ']',
+            ['<'] = '>',
+        };
+
+        var symbols = new Stack<char>();
+        foreach (var @char in line)
+        {
+            if (closingSymbols.ContainsKey(@char))
+            {
+                symbols.Push(@char);
+                continue;
+            }
+
+            var expectedClosing = closingSymbols[symbols.Pop()];
+            if (closingSymbols.Values.Contains(@char) && expectedClosing != @char)
+            {
+                return @char;
+            }
+        }
+
+        return null;
+    }
+
+    public int PartOne(IEnumerable<string> input)
+    {
+        return input
+            .Select(GetCorruptingChar)
+            .Sum(corruptingChar => corruptingChar switch
+            {
+                ')' => 3,
+                ']' => 57,
+                '}' => 1197,
+                '>' => 25137,
+                _ => 0,
+            });
+    }
+
+    public int PartTwo(IEnumerable<string> input)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public IEnumerable<string> ReadInput(string inputPath)
+        => File.ReadLines(inputPath);
+}
