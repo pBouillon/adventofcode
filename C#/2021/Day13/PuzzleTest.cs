617c5e6be6471f2d0afcc8e2228e7fc8115c950f
pBouillon <pro.pierre.bouillon@protonmail.com>
2021-12-13 06:40:49 +0100

    [skip-ci] - Parse the input of day 13 of 2021

---
+++ C#/2021/Day13/PuzzleTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+using TestCommons;
+
+namespace _2021.Day13;
+
+public class PuzzleTest : SolverTest<Solver, (Dictionary<Coordinate, bool>, IEnumerable<FoldingInstruction>), int>
+{
+    protected override Example PartOne => new()
+    {
+        Input = (new Dictionary<Coordinate, bool>(), Array.Empty<FoldingInstruction>()),
+        Result = 17,
+    };
+
+    protected override int PartOneSolution => 0;
+
+    protected override Example PartTwo => new()
+    {
+        Input = (new Dictionary<Coordinate, bool>(), Array.Empty<FoldingInstruction>()),
+        Result = 0,
+    };
+
+    protected override int PartTwoSolution => 0;
+}
---
+++ C#/2021/Day13/Solver.cs
@@ -0,0 +1,68 @@
+﻿using Commons;
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _2021.Day13;
+
+public enum Axis { X, Y}
+
+public record Coordinate(int X, int Y);
+
+public record FoldingInstruction(Axis Direction, int Offset);
+
+public class Solver : ISolver<(Dictionary<Coordinate, bool>, IEnumerable<FoldingInstruction>), int>
+{
+    public string InputPath => "Day13.txt";
+
+    public int PartOne((Dictionary<Coordinate, bool>, IEnumerable<FoldingInstruction>) input)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public int PartTwo((Dictionary<Coordinate, bool>, IEnumerable<FoldingInstruction>) input)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public (Dictionary<Coordinate, bool>, IEnumerable<FoldingInstruction>) ReadInput(string inputPath)
+    {
+        var points = File
+            .ReadLines(inputPath)
+            .TakeWhile(line => !string.IsNullOrWhiteSpace(line))
+            .Select(line =>
+            {
+                var split = line
+                    .Split(',')
+                    .Select(int.Parse)
+                    .ToArray();
+
+                return new Coordinate(split[0], split[1]);
+            })
+            .ToDictionary(
+                coordinate => coordinate,
+                coordinate => true);
+
+        var instructions = File
+            .ReadLines(inputPath)
+            .SkipWhile(line => char.IsDigit(line[0]) || string.IsNullOrWhiteSpace(line))
+            .Select(line =>
+            {
+                var matches = Regex
+                    .Match(line, @"fold along ([xy])=(\d)")
+                    .Groups
+                    .Cast<Group>()
+                    .Skip(1)
+                    .Select(group => group.Value)
+                    .ToArray();
+
+                return new FoldingInstruction(
+                    matches[0] == "x" ? Axis.X : Axis.Y,
+                    int.Parse(matches[1]));
+            });
+
+        return (points, instructions);
+    }
+}
