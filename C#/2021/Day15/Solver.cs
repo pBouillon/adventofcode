41a1e74a02bb503e06bcbcaf5962bebf0e8e4463
pBouillon <pro.pierre.bouillon@protonmail.com>
2021-12-15 06:19:17 +0100

    [skip ci] Prepare day 15

---
+++ C#/2021/Day15/Solver.cs
@@ -0,0 +1,27 @@
+﻿using Commons;
+
+using System.IO;
+using System.Linq;
+
+namespace _2021.Day15;
+
+public class Solver : ISolver<int[][], long>
+{
+    public string InputPath => "Day15/input.txt";
+
+    public long PartOne(int[][] input)
+    {
+        return 0;
+    }
+
+    public long PartTwo(int[][] input)
+    {
+        return 0;
+    }
+
+    public int[][] ReadInput(string inputPath)
+        => File
+            .ReadLines(inputPath)
+            .Select(line => line.Select(x => x - '0').ToArray())
+            .ToArray();
+}
