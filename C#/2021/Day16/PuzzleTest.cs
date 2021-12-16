618cbee03182cf6340f9ec7186c5ca92656f23ea
pBouillon <pro.pierre.bouillon@protonmail.com>
2021-12-16 06:22:42 +0100

    [skip ci] Prepare the sixteenth day of 2021

---
+++ C#/2021/Day16/PuzzleTest.cs
@@ -0,0 +1,18 @@
+﻿using TestCommons;
+
+namespace _2021.Day16;
+
+public class PuzzleTest : SolverTest<Solver, string, int>
+{
+    protected override Example PartOne => new()
+    {
+        Input = "A0016C880162017C3686B18A3D4780",
+        Result = 31,
+    };
+
+    protected override int PartOneSolution => 0;
+
+    protected override Example PartTwo => throw new System.NotImplementedException();
+
+    protected override int PartTwoSolution => 0;
+}
---
+++ C#/2021/Day16/Solver.cs
@@ -0,0 +1,24 @@
+﻿using Commons;
+
+using System.IO;
+using System.Linq;
+
+namespace _2021.Day16;
+
+public class Solver : ISolver<string, int>
+{
+    public string InputPath => "Day16/input.txt";
+
+    public int PartOne(string transmission)
+    {
+        return 0;
+    }
+
+    public int PartTwo(string transmission)
+    {
+        return 0;
+    }
+
+    public string ReadInput(string inputPath)
+        => File.ReadLines(inputPath).First();
+}
